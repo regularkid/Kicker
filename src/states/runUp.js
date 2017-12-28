c06797357c4ceb80a50ccf7e97c53e7bf39dfb64
Perfetto <perfetto@amazon.com>
2017-12-28 10:34:10 -0800

    Fixed issue with blocked logic and kick logic potentially firing on the same frame.

--- src/states/runUp.js
+++ src/states/runUp.js
@@ -29,7 +29,7 @@ function runUp()
     updateCrowd();
     updatePlayers();
     
-    if (runTime > 0.0)
+    if (runTime > 0.0 && g.state != inAir)
     {
         updatePowerMeter();
         setKickPlayerPos(1.0 - runTime);
